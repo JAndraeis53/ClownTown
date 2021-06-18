1983a7ed36837d9f133b801bb6b39a4cd71f27fa
Jalon Duncan <duncanjalon@gmail.com>
2021-06-18 14:41:56 -0500

    finished project

--- src/scripts/buttonsClown.js
+++ src/scripts/buttonsClown.js
@@ -1,16 +1,16 @@
-// import { RequestForm } from "./RequestForm.js"
-// import { partyOrder } from "./partyOrder.js"
+import { RequestForm } from "./RequestForm.js"
+import { partyOrder } from "./partyOrder.js"
 
 export const buttonsClown = () => {
     return `
         <h1>Hire Buttons and Lollipop the Clowns</h1>
         <section class="requestForm">
-            {RequestForm()}
+            ${RequestForm()}
         </section>
 
         <section class="partyOrders">
             <h2>Order a Party today!</h2>
-            {partyOrder()}
+            ${partyOrder()}
         </section>
     `
 }
